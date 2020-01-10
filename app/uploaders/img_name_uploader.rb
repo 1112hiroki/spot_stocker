79dd8833bb9ffded2b044bcfdb834386c705f281
hiroki <1112hiroki.suzuki@gmail.com>
2020-01-10 09:57:14 +0900

    fogの追加

--- app/uploaders/img_name_uploader.rb
+++ app/uploaders/img_name_uploader.rb
@@ -2,7 +2,14 @@ class ImgNameUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  storage :file
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   version :thumb do
     process resize_to_fit: [400, 400]
@@ -11,7 +18,6 @@ class ImgNameUploader < CarrierWave::Uploader::Base
   version :thumb_header do
     process resize_to_fit: [80, 80]
   end
-
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -23,6 +29,10 @@ class ImgNameUploader < CarrierWave::Uploader::Base
 
   def extension_whitelist
     %w(jpg jpeg gif png)
+  end
+
+  def filename
+    original_filename if original_filename
   end
 
 end
