bd7a4a1903517b8809a41ef7f614476089151207
hiroki <1112hiroki.suzuki@gmail.com>
2020-01-21 20:43:45 +0900

    user_spec.rbの追加

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'ログイン機能' do
+describe 'ユーザーログイン' do
 
   describe 'ログインする' do
     before do
@@ -10,7 +10,24 @@ describe 'ログイン機能' do
       click_button "ログイン"
     end
 
-    it '投稿一覧へ遷移する' do
+    it 'ログイン後、投稿一覧へ遷移する' do
+      expect(page).to have_content 'スポット一覧'
+    end
+  end
+
+  describe '新規登録する' do
+    before do
+      visit new_user_registration_path
+      fill_in 'ユーザー名', with: "さとう"
+      fill_in 'メールアドレス', with: "sato@example.com"
+      choose 'user_sex_男性'
+      choose 'user_age_１０代'
+      fill_in 'パスワード', with: "password"
+      fill_in 'パスワード再入力', with: "password"
+      click_button "アカウントを作成"
+    end
+
+    it '登録後、投稿一覧へ遷移する' do
       expect(page).to have_content 'スポット一覧'
     end
   end
